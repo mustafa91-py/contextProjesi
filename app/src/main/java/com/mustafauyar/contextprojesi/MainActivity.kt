100dfb09c5692027f8c50a9860c7299cbafef1eb
Mustafa2 <Mustafauyar91@gmail.com>
2024-01-07 14:14:09 +0300

    contextProjesi ders5_5

---
+++ app/src/main/java/com/mustafauyar/contextprojesi/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.mustafauyar.contextprojesi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.mustafauyar.contextprojesi.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var tost_mesaj = Toast.makeText(this,"Hoşgeldiniz",Toast.LENGTH_LONG)
+        tost_mesaj.show()
+    }
+    fun mesajGoster(view: View){
+    }
+}
