5ad465d9baaa3bc8dabfc178802c77b4f83182fd
Mustafa2 <Mustafauyar91@gmail.com>
2024-01-07 14:45:29 +0300

    ders5_7 bitti

--- app/src/main/java/com/mustafauyar/contextprojesi/MainActivity.kt
+++ app/src/main/java/com/mustafauyar/contextprojesi/MainActivity.kt
@@ -1,23 +1,42 @@
 package com.mustafauyar.contextprojesi
 
+import android.app.Dialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.mustafauyar.contextprojesi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
-    private lateinit var tost_mesaj:Toast
+    private lateinit var toastMesaj:Toast
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        tost_mesaj = Toast.makeText(this,"Hoşgeldiniz",Toast.LENGTH_LONG)
+        toastMesaj = Toast.makeText(this,"Hoşgeldiniz",Toast.LENGTH_LONG)
 //        tost_mesaj.show()
     }
     fun mesajGoster(view: View){
-        tost_mesaj.show()
+        /*
+        1 - AlertDialog.Builder da this kullanmak mantıklı yoksa çöküyor
+        2 - Toast da ise yeni toast oluştur aynı toast dan oluşturusan süreler den dolayı gösterilmiyor
+         */
+        val uyariMesaji = AlertDialog.Builder(this)
+        uyariMesaji.setTitle("şifre hatası")
+        uyariMesaji.setMessage("şifre hatası, tekrar denemek ister misiniz ?")
+        uyariMesaji.setPositiveButton("evet".uppercase(),DialogInterface.OnClickListener { dialog, which ->
+            toastMesaj.setText("baştan deniyorusunuz")//sıfın toastı kullandım
+            toastMesaj.show()
+        })
+        uyariMesaji.setNegativeButton("hayır") {dialog,i ->
+            Toast.makeText(this,"hayırlısı artık",Toast.LENGTH_LONG).show() // yeni toast oluşturdum
+//            toastMesaj.setText("hayırlısı artık")
+//            toastMesaj.show()
+        }
+        uyariMesaji.show()
     }
 }
